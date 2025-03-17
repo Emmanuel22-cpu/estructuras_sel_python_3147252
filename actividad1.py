da67c2e2b15649cef31adf63d0997eeb4338ae51
carlos <nbayona000@gmail.com>
2025-03-17 08:54:22 -1200

    if simple

---
+++ actividad1.py
@@ -0,0 +1,6 @@
+temperatura = int(input("Digite la temperatura de su motor: "))
+if temperatura > 80:
+    print("Apagado")
+else:
+     print("Encendido")
+print("Fin del programa")
--- if.py
+++ if.py
@@ -4,10 +4,14 @@ Se utiliza para tomar decisiones
 basadas en expresiones condicionales
 '''
 #ejemplo 1: if SIMPLE
-edad = 19
+edad = int(input("Ingresa tu edad: "))
+documento =input("Tienes Cedula? (si/no):")
 #condicional: si la edad es amyor o igual a 18 
-if edad > 18:python if.py
+if edad >= 18 and documento=='si':
     #codigo para cuando es mayor a 18 
-    print("Eres mayor de edad, puedes ir a farrear") 
+    print("Eres mayor de edad y tienes documento")
+else:
+    #codigo para cuando es menor a 18
+    print("Eres menor de edad o no tienes documento")
 #codigo que se ejecuta siempre 
 print("Fin del programa")
