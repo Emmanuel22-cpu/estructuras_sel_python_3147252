05162b4f8e49518480f5789be50f03c80f6f6ef0
carlos <nbayona000@gmail.com>
2025-03-17 07:28:00 -1200

    if

---
+++ if.py
@@ -0,0 +1,13 @@
+'''
+Estructura de control if:
+Se utiliza para tomar decisiones 
+basadas en expresiones condicionales
+'''
+#ejemplo 1: if SIMPLE
+edad = 19
+#condicional: si la edad es amyor o igual a 18 
+if edad > 18:python if.py
+    #codigo para cuando es mayor a 18 
+    print("Eres mayor de edad, puedes ir a farrear") 
+#codigo que se ejecuta siempre 
+print("Fin del programa")
